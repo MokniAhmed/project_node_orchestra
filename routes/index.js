17c377daeac11f72310f0f3c08ec86287d815a6a
MokniAhmed <mokni.ahmed.am@gmail.com>
2023-11-29 16:23:38 +0100

    add concidate route path

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,12 @@
 
 const userRoute = require('./userRoute');
 const authRoute = require('./authRoute');
-
+const condidateRoute = require("./condidateRoute");
 
 const mountRoutes = (app) => {
- 
-  app.use('/api/v1/users', userRoute);
-  app.use('/api/v1/auth', authRoute);
- 
+  app.use("/api/v1/users", userRoute);
+  app.use("/api/v1/auth", authRoute);
+  app.use("/api/v1/condidate", condidateRoute);
 };
 
 module.exports = mountRoutes;
