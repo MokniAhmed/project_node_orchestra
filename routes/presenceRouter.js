6d11240ed94ccfb131085522b50f151bb904dd9a
khàlyl Ouchéry <khalilouchari12@gmail.com>
2023-12-10 19:29:54 +0100

    demand_absence_khalil_20

--- routes/presenceRouter.js
+++ routes/presenceRouter.js
@@ -2,11 +2,13 @@ const express = require("express");
 const {
   markPrsence,
   addPrsenceManualy,
+  demandeAbsent,
 } = require("../services/presenceService");
 const { protect, allowedTo } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 router.post("/qrcode/:id", protect, allowedTo("chorist"), markPrsence);
 router.post("/add-manualy/:id", addPrsenceManualy);
+router.put("/demandeAbsent", protect, demandeAbsent);
 
 module.exports = router;
