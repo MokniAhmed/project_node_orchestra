4c9cfab0cd926008e89a09e3ae93a612b2363646
MokniAhmed <mokni.ahmed.am@gmail.com>
2023-12-10 17:37:17 +0100

    add day date to concerte model

--- models/concertModel.js
+++ models/concertModel.js
@@ -6,9 +6,10 @@ const concertSchema = new mongoose.Schema(
     season: { type: mongoose.Schema.ObjectId, ref: "Season" },
     name: { type: String, required: [true, "concert need a Name."] },
     location: String,
+    day: Date,
     description: String,
     music: [{ type: mongoose.Schema.ObjectId, ref: "Musical" }],
-   //// list_final: [{ type: mongoose.Schema.ObjectId, ref: "User" }],
+    //// list_final: [{ type: mongoose.Schema.ObjectId, ref: "User" }],
   },
   { timestamps: true }
 );
