64d615cf5516e1572f2fa1b6740d183a3a82f452
MokniAhmed <mokni.ahmed.am@gmail.com>
2023-12-10 18:52:18 +0100

    update createRepetition

--- services/presenceService.js
+++ services/presenceService.js
@@ -10,10 +10,7 @@ exports.markPrsence = asyncHandler(async (req, res, next) => {
   const repetition = await Repetition.findById(req.params.id);
   if (!repetition)
     next(new ApiError("there s no repetition with this ID.", 400));
-  /* const { end_rep: endingRep } = repetition;
-  if (Date.now() > endingRep + 5 * 60 * 1000) {
-    next(new ApiError("this no valid anymore ", 400));
-  }*/
+
   repetition.list_presence.push(req.user.id);
   await repetition.save();
   res
@@ -35,3 +32,8 @@ exports.addPrsenceManualy = asyncHandler(async (req, res, next) => {
   await repetition.save();
   res.status(200).json({ message: "presence has been added sucessfulyy " });
 });
+
+// @desc    demande absent to rep or concert
+// @route   POST /api/v1/presence/demandeAbsent
+// @access  private/chorist
+exports.demandeAbsent = asyncHandler(async (req, res, next) => {});
